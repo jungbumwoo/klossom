eb5cdb3c5a66991d5d3de27836186dcd2a700339
jungbum <jungbumwoo@naver.com>
2020-09-24 11:16:25 +0900

    onlyPublic, onlyPrivate

--- server/src/routers/VideoRouter/videoRouter.js
+++ server/src/routers/VideoRouter/videoRouter.js
@@ -4,17 +4,19 @@ import { upload, postUpload, videoDetail, getEditVideo, postEditVideo,
     postDeleteVideo,
     getDeleteVideo } from "../../controller/videoController";
 import { uploadVideo } from "../../../middlewares";
+import { onlyPrivate } from "../../../middlewares";
+
 const videoRouter = express.Router();
 
 console.log(routes.deleteVideo);
 console.log(routes.deleteVideo());
 
-videoRouter.get(routes.upload, upload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, upload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
-videoRouter.get(routes.deleteVideo(), getDeleteVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, getDeleteVideo);
 
 
 export default videoRouter;
