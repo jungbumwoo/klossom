f475393c4a86b7c85e7fbeb20897474ab17be413
jungbum <jungbumwoo@naver.com>
2020-09-28 14:13:02 +0900

    #7.3 Adding Creator to Video

--- server/src/controller/videoController.js
+++ server/src/controller/videoController.js
@@ -40,7 +40,10 @@ export const postUpload = async (req, res) => {
     fileUrl: file.path,
     title,
     description,
+    creator: req.user.id
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -49,7 +52,7 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
     res.render("videoDetail", { pageTitle: "Video Detail", video });
   } catch (err) {
     console.log(`error is occured. in videoDetail, videoController!`);
